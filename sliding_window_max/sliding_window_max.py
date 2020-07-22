7f4d67497a19ffe76a080bf07ad379b5854e4f0a
Blayze <blayzestone@protonmail.com>
2020-07-22 16:16:48 -0700

    implement first pass for sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,15 +1,31 @@
-'''
+"""
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
+"""
+
+
 def sliding_window_max(nums, k):
-    # Your code here
+    result = []
 
-    pass
+    for i in range(len(nums)):
+        greatest_num = None
+
+        if i + k > len(nums):
+            break
+
+        for j in range(i, i + k):
+            if greatest_num is None:
+                greatest_num = nums[j]
+            elif nums[j] > greatest_num:
+                greatest_num = nums[j]
+
+        result.append(greatest_num)
+
+    return result
 
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 

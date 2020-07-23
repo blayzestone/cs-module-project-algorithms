c12ef3321f94966a966e18dd003423c1cc71e609
Blayze <blayzestone@protonmail.com>
2020-07-23 09:56:15 -0700

    implement 'eating_cookies' solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,40 @@
-'''
+"""
 Input: an integer
 Returns: an integer
-'''
-def eating_cookies(n):
-    # Your code here
+"""
 
-    pass
+
+# def eating_cookies(n, p):
+
+#     if n <= 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+
+#     return (
+#         eating_cookies(n - 1, p) + eating_cookies(n - 2, p) + eating_cookies(n - 3, p)
+#     )
+
+
+def eating_cookies(n, p=[]):
+    if n <= 1:
+        return 1
+    else:
+        num_ways = [0] * (n + 1)
+        num_ways[0] = 1
+        num_ways[1] = 1
+        num_ways[2] = 2
+
+        for i in range(3, n + 1):
+            num_ways[i] = num_ways[i - 1] + num_ways[i - 2] + num_ways[i - 3]
+
+        return num_ways[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 3
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies"
+    )
